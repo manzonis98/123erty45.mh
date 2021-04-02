a2b8db41bec684d2d9c5d9a11a71e0bdf238e38e
manzonis98 <63615231+manzonis98@users.noreply.github.com>
2021-04-02 22:48:07 +0200

    Spedd Aiuto Ball

--- Prova2/Assets/Movement.cs
+++ Prova2/Assets/Movement.cs
@@ -5,10 +5,11 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
 
     public Rigidbody rb;
+    public float speed = 1000f;
 
     void Start()
     {
-        rb.AddForce(1000 * Time.deltaTime, 0, 0);
+        rb.AddForce(speed * Time.deltaTime, 0, 0);
     }
 
     // Update is called once per frame
